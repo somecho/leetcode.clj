d0294c81642b649a901fbd3d4fb9cb803f28958f
Somē Cho <itssomicho@gmail.com>
2023-05-27 12:24:45 +0200

    problem 1 - two sum

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Leetcode problems solved in Clojure.
 
 ## Problems
 ### [Easy](./src/problems/easy.clj)
+1 - Two Sum
 169 - Majority Element
 
 ## Testing
--- src/problems/easy.clj
+++ src/problems/easy.clj
@@ -1,16 +1,38 @@
 (ns problems.easy)
 
-(defn majorityElement
-  "
-  169. Majority Element
+(defn two-sum
+  "1. Two Sum
+  https://leetcode.com/problems/two-sum/
+
+  Given an array of integers `nums` and an integer `target`,
+  return indices of the two numbers such that they add up
+  to `target`.
+
+  You may assume that each input would have exactly one
+  solution, and you may not use the same element twice.
+
+  You can return the answer in any order."
+  [nums target]
+  (-> (map-indexed #(vec [%1 %2]) nums)
+      (as-> indexed
+            (for [x indexed
+                  y indexed]
+              (when (and (= (+ (last x) (last y)) target)
+                         (not= (first x) (first y)))
+                [x y])))
+      (as-> pairs (filter not-empty pairs))
+      (first)
+      (as-> pair (map first pair))))
+
+(defn majority-element
+  "169. Majority Element
   https://leetcode.com/problems/majority-element/
 
   Given an array nums of size `n`, return the majority element.
 
   The majority element is the element that appears more than
   `⌊n / 2⌋` times. You may assume that the majority element
-  always exists in the array.
-  "
+  always exists in the array."
   [nums]
   (->> nums
        (frequencies)
